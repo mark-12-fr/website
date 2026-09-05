5d4e3fcd277d27f51a8c6bbae5f6a0f1c4f79c4d
Mark Frizas <markrealmec2@gmail.com>
2026-09-05 19:36:53 +0800

    add sign up form

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,41 @@
 const express = require('express');
 const app = express();
-const db_connection = require('./db/connection');
+const db_connection = require('./db_connection');
 
+app.set('view engine', 'ejs');
+app.use(express.static('frontend'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index', {title: "Sign Up"});
+});
+
+app.get('/signup', (req, res) => {
+    res.render('index', {title: "Sign Up"});
+});
+
+app.post('/signupprocess', (req, res) => {
+    const id = 0;
+    const username = req.body.username;
+    const password = req.body.password;
+    
+
+    const sql = `INSERT INTO users VALUES (
+    "${id}",
+    "${username}",
+    "${password}"
+    )`;
+
+    db_connection.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(
+            '<script>alert("User registered successfully!"); window.location.href = "/";</script>');
+        });
+    });
+
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
