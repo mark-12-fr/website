42a132283f88fba7a430d83542a408add51293e1
Mark Frizas <markrealmec2@gmail.com>
2026-09-05 21:22:05 +0800

    hsdhjsd

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # website
+
+## Setup Instructions
+
+1. Install dependencies:
+```bash
+npm install
+```
+
+2. Create a `.env` file in the root folder with the following:
+```
+SUPABASE_URL=your_supabase_url_here
+SUPABASE_KEY=your_supabase_key_here
+```
+
+3. Run the server:
+```bash
+npm start
+```
+
+## Environment Variables
+
+| Variable | Description |
+|----------|-------------|
+| `SUPABASE_URL` | Your Supabase project URL |
+| `SUPABASE_KEY` | Your Supabase anon/public key |
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db_connection = require('./db_connection');
+const supabase = require('./db_connection');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -13,21 +13,25 @@ app.get('/', (req, res) => {
     res.render('index', {title: "Sign Up"});
 });
 
-app.post('/signupprocess', (req, res) => {
+app.get('/signup', (req, res) => {
+    res.render('index', {title: "Sign Up"});
+});
+
+app.post('/signupprocess', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    
 
-   const sql = `INSERT INTO users (username, password) VALUES (?, ?)`;
+    const { data, error } = await supabase
+        .from('users')
+        .insert([{ username, password }]);
 
+    if (error) {
+        res.send('<script>alert("Error!"); window.location.href = "/";</script>');
+    } else {
+        res.send('<script>alert("User registered successfully!"); window.location.href = "/";</script>');
+    }
+});
 
-    db_connection.query(sql,[username, password], (err, result) => {
-        if (err) throw err;
-        res.send(
-            '<script>alert("User registered successfully!"); window.location.href = "/";</script>');
-        });
-    });
-
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
